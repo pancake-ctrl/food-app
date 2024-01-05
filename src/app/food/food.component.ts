1ba1a561d34f125a63845a243bbcef41809d6f57
Azrar <azrar2277@gmail.com>
2024-01-05 11:56:25 +0800

    filter food list and json db set up

--- src/app/food/food.component.ts
+++ src/app/food/food.component.ts
@@ -11,14 +11,14 @@ import { MenuCardComponent } from '../menu-card/menu-card.component';
   template: ` 
   <section class="mb-12">
       <form class="flex justify-center sm:justify-start">
-        <input class="border-2 border-black-400 h-[40px] " type="text" placeholder="Filter by city" />
-        <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
+        <input class="border-2 border-black-400 h-[40px] " type="text" placeholder="Filter by city" #filter/>
+        <button type="button" (click)="filterResults(filter.value)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Search</button>
       </form>
     </section>
     <section class="results h-screen flex flex-row flex-wrap sm:justify-start justify-center">
       <app-menu-card class="h-fit block" 
-      *ngFor="let a of menuDataList"
-        [menuData]="a">
+      *ngFor="let food of filteredFoodList"
+        [menuData]="food">
     </app-menu-card>
   </section>
     `,
@@ -29,12 +29,21 @@ export class FoodComponent {
   readonly baseUrl = '/assets';
 
 
-  menuDataList: MenuData [] = [ ]
-
+  menuDataList: MenuData [] = [];
+  filteredFoodList: MenuData [];
   informationService: InformationService = inject(InformationService)
 
   constructor() {
     this.menuDataList = this.informationService.getAllMenuData();
+    this.filteredFoodList = this.menuDataList;
+  }
+  
+  filterResults(text:String){
+    if(!text){
+      this.filteredFoodList = this.menuDataList;
+    }
+
+    this.filteredFoodList = this.menuDataList.filter(food => food?.name.toLowerCase().includes(text.toLowerCase()))
   }
 
   
